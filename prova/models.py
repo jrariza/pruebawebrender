4b93926730803d1982b61a1518a86959df1d9387
jrariza <joseramonarizaperez@gmail.com>
2023-04-05 14:19:40 +0200

    prueba

---
+++ prova/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    birth_country = models.CharField(max_length=50)
+    
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=15)
+    
+
+class Language(models.Model):
+    name = models.CharField(max_length=15)
+    
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    name = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+    sinopsys = models.CharField(max_length=200)
+    genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING)
+    language = models.ForeignKey(Language, on_delete=models.DO_NOTHING)
+
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=15)
